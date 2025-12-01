5b008a9dca5636e17a9c2b6578ebd0f131ae8a77
muunuo <benkforland@gmail.com>
2025-12-01 14:11:54 +0100

    kommentarer for anbefaling
    kommentarer for anbefaling kode er ferdig.
    Plan er å stilsette ting litt. Gjøre at bio kun er syneligt med hover og andre slike ting

--- beskyttet/brukerInfo.js
+++ beskyttet/brukerInfo.js
@@ -1,5 +1,11 @@
 //Viser brukernavn på toppen av siden
 
+/*
+-------------------------------
+    BRUKERNAVN
+-------------------------------
+*/
+
 (async function () {
     const params = new URLSearchParams(window.location.search); //hent noe fra søkefeltet
     const id = params.get("id"); //hent id fra søkefeltet
@@ -17,6 +23,13 @@
     }
 })(); 
 
+
+/*
+-------------------------------
+    ANBEFALT
+-------------------------------
+*/
+
     (async function () {
     const params = new URLSearchParams(window.location.search); //hent noe fra søkefeltet
     const mottakerID = params.get("id"); //hent id fra søkefeltet
@@ -27,27 +40,28 @@
         if (!hent.ok) throw new Error("Feil ved henting av bruker"); //hvis ingen id fra bruker tabell
         const anbefaling = await hent.json();
 
-        const vis = document.querySelector("#anbefalt");
-        if (vis) {
-            for (const item of anbefaling) {
+        const vis = document.querySelector("#anbefalt"); //heter div med id anbefalt 
+        if (vis) { //om anbefalt vises
+            for (const an of anbefaling) { //for hver ting innenfor anvefaling lages følgende: //an = anbefalt av venner
                 const serieDiv = document.createElement('div');
-                serieDiv.classList.add('serieKonteiner');
+                serieDiv.classList.add('serieKonteiner');// oppretter en klasse som heter serieKontainer
+                // alt under legges inn i en serie konteiner (background color i css for å se)
 
                 const h3 = document.createElement('h3');
-                h3.textContent = `${item.navn} (${item.ar})`;
+                h3.textContent = `${an.navn} (${an.ar})`;
                 serieDiv.appendChild(h3);
 
                 const p = document.createElement('p');
-                p.textContent = `${item.bio}`;
+                p.textContent = `${an.bio}`;
                 serieDiv.appendChild(p);
 
                 const kommentar = document.createElement('p');
-                kommentar.textContent = `Kommentar: ${item.kommentar}`;
+                kommentar.textContent = `Kommentar: ${an.kommentar}`;
                 serieDiv.appendChild(kommentar);
 
-                if (item.plakat) {
+                if (an.plakat) {
                     const bilde = document.createElement('img'); //lager bildet
-                    bilde.src = item.plakat //henter bilde fra serie.plakat
+                    bilde.src = an.plakat //henter bilde fra serie.plakat
                     bilde.alt = serie.navn;
                     bilde.style.width = '120px';
                     serieDiv.appendChild(bilde);
