23e1745a24243c58541b59edc98034353dfd5b26
muunuo <benkforland@gmail.com>
2026-03-06 11:27:28 +0100

    Fikset et problem, fikk et nytt
    Fikset et problem hvor du ikke kunne logge inn. Du kan nå logge inn, men bruker sitt brukernavn, og eventuelle serier er ikke synelige så langt

--- app.js
+++ app.js
@@ -18,9 +18,14 @@ const PORT = 3000;
 */
 app.use(express.static('public')); // lar oss hente fra public mappen
 app.use(express.json()); // parse (løse) json fra request body
-
-// app.use(express.static('beskyttet')); //sier de kan hente fra beskyttet (må endres om beskyttet noen gang blir beskyttet)
 app.use(express.urlencoded({ extended: true })); //lar oss hente ting fra søkebaren
+
+app.use(session({
+    secret: 'velg-en-hemmelig-nøkkel',
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false } // secure: true kun med https
+}));
 
 const path = require('path'); //for å håntere filstier
 
@@ -91,12 +96,6 @@ app.get("/", (req, res) => {
 -------------------------------
 */
 
-// app.get("/api/minside",kreverInnlogging, (req, res) => {
-//     const brukerId = req.session.bruker.id;
-//     const bruker = db.prepare("SELECT id, brukernavn, navn, passord FROM bruker WHERE id = ?").get(brukerId);
-//     res.json({ bruker });
-// });
-
 app.post('/opprettKonto', async (req, res) => { // Oppretter ny brukerkonto
     const { brukernavn, navn, passord } = req.body;
     
@@ -120,6 +119,12 @@ app.post('/opprettKonto', async (req, res) => { // Oppretter ny brukerkonto
     }
 });
 
+app.get("/api/dashbord",kreverInnlogging, (req, res) => {
+    const brukerId = req.session.bruker.id;
+    const bruker = db.prepare("SELECT id, brukernavn, navn, passord FROM bruker WHERE id = ?").get(brukerId);
+    res.json({bruker});
+});
+
 app.post('/innlogget', async (req, res)=> { // Lar deg logge inn
     const {brukernavn, passord} = req.body; //henter brukernavn og passord fra body
     
@@ -132,139 +137,145 @@ app.post('/innlogget', async (req, res)=> { // Lar deg logge inn
     if (!gyldigPassord) {
         return res.status(401).json({ message: "feil brukernavn eller passord"});
     }
+
+    req.session.bruker = { id: bruker.id, brukernavn: bruker.brukernavn };
+    return res.redirect("/beskyttet/dashbord.html");
+
+
     // req.session.bruker = { id: bruker.id, brukernavn: bruker.brukernavn };
-    res.json("Innlogget vellykket"); //hvis det fungerer får du få videre
+    // res.json(bruker); //hvis det fungerer får du gå videre
+    // res.redirect("/dashbord");
+});
+
+app.get('/bruker/:id', (req, res) => { //Vise frem brukernavn når innlogget
+    const id = req.params.id;
+    const brukerInfo = db.prepare('SELECT id, brukernavn, navn FROM bruker WHERE id = ?').get(id); //Henter ut info fra bruker med x id.
+    if (!brukerInfo) return res.status(404).json({ message: "bruker ikke funnet" }); //hvis noe mangler: Feilmelding
+    res.json(brukerInfo);
 });
 
 // app.post("/api/logout", (req, res) => {
 //     req.session.destroy();
 //     res.json({message: "Du er nå logget ut"});
-// }) 
-
-// app.get('/bruker/:id', (req, res) => { //Vise frem brukernavn når innlogget
-//     const id = req.params.id;
-//     const brukerInfo = db.prepare('SELECT id, brukernavn, navn FROM bruker WHERE id = ?').get(id); //Henter ut info fra bruker med x id.
-//     if (!brukerInfo) return res.status(404).json({ message: "bruker ikke funnet" }); //hvis noe mangler: Feilmelding
-//     res.json(brukerInfo);
 // });
 
-// /*
-// -------------------------------
-//     SERIE
-// -------------------------------
-// */
-// app.post('/opprettSerie', (req, res)=> { // oppretter en serie
-//     const { serieNavn, serieBio, seriePlakat } = req.body;
+/*
+-------------------------------
+    SERIE
+-------------------------------
+*/
+app.post('/opprettSerie', (req, res)=> { // oppretter en serie
+    const { serieNavn, serieBio, seriePlakat } = req.body;
 
-//     try {
-//         console.log(serieNavn, serieBio, seriePlakat)
-//         const insert = db.prepare('INSERT INTO serie (navn, bio, plakat) VALUES (?, ?, ?)');
-//         insert.run(serieNavn, serieBio, seriePlakat);
-//     } catch (error) {
-//         console.log(error);
-//         res.send("serie ved opprettelse"); //sender meldingen om catch eller try blir trigget
-//     }
-// });
+    try {
+        console.log(serieNavn, serieBio, seriePlakat)
+        const insert = db.prepare('INSERT INTO serie (navn, bio, plakat) VALUES (?, ?, ?)');
+        insert.run(serieNavn, serieBio, seriePlakat);
+    } catch (error) {
+        console.log(error);
+        res.send("serie ved opprettelse"); //sender meldingen om catch eller try blir trigget
+    }
+});
 
-// app.get('/alleSerier', (req, res) => { //viser alle serier i serie.html
-//     const serieListe = db.prepare('SELECT * FROM serie').all();
+app.get('/alleSerier', (req, res) => { //viser alle serier i serie.html
+    const serieListe = db.prepare('SELECT * FROM serie').all();
 
-//     res.json(serieListe);
+    res.json(serieListe);
     
-// });
+});
 
-// /*
-// -------------------------------
-//     sett/ser
-// -------------------------------
-// */
-// app.get('/harSett/:id', (req, res) => { 
-//     const person = req.params.id;
-//     const sjekkStatus = db.prepare(`
-//         SELECT serieStatus.*, serie.*
-//         FROM serieStatus
-//         INNER JOIN serie ON serieStatus.idS = serie.idS 
-//         WHERE status = 'sett'
-//         AND serieStatus.idB = ?
-//         `).all(person)
+/*
+-------------------------------
+    sett/ser
+-------------------------------
+*/
+app.get('/harSett/:id', (req, res) => { 
+    const person = req.params.id;
+    const sjekkStatus = db.prepare(`
+        SELECT serieStatus.*, serie.*
+        FROM serieStatus
+        INNER JOIN serie ON serieStatus.idS = serie.idS 
+        WHERE status = 'sett'
+        AND serieStatus.idB = ?
+        `).all(person)
 
-//     res.json(sjekkStatus)
+    res.json(sjekkStatus)
 
-// });//Må søke opp: http://localhost:3000/harSett/18?status=sett
+});//Må søke opp: http://localhost:3000/harSett/18?status=sett
 
-// app.get('/serPa/:id', (req, res) => { 
-//     const person = req.params.id;
-//     const sjekkStatus = db.prepare(`
-//         SELECT serieStatus.*, serie.*
-//         FROM serieStatus
-//         INNER JOIN serie ON serieStatus.idS = serie.idS 
-//         WHERE status = 'ser'
-//         AND serieStatus.idB = ?
-//         `).all(person)
+app.get('/serPa/:id', (req, res) => { 
+    const person = req.params.id;
+    const sjekkStatus = db.prepare(`
+        SELECT serieStatus.*, serie.*
+        FROM serieStatus
+        INNER JOIN serie ON serieStatus.idS = serie.idS 
+        WHERE status = 'ser'
+        AND serieStatus.idB = ?
+        `).all(person)
 
-//     res.json(sjekkStatus)
+    res.json(sjekkStatus)
 
-// });
+});
 
-// app.get('/vilSe/:id', (req, res) => { 
-//     const person = req.params.id;
-//     const sjekkStatus = db.prepare(`
-//         SELECT serieStatus.*, serie.*
-//         FROM serieStatus
-//         INNER JOIN serie ON serieStatus.idS = serie.idS 
-//         WHERE status = 'vil'
-//         AND serieStatus.idB = ?
-//         `).all(person)
+app.get('/vilSe/:id', (req, res) => { 
+    const person = req.params.id;
+    const sjekkStatus = db.prepare(`
+        SELECT serieStatus.*, serie.*
+        FROM serieStatus
+        INNER JOIN serie ON serieStatus.idS = serie.idS 
+        WHERE status = 'vil'
+        AND serieStatus.idB = ?
+        `).all(person)
 
-//     res.json(sjekkStatus)
+    res.json(sjekkStatus)
 
-// });
+});
 
-// /*
-// -------------------------------
-//     ANBEFALINGER
-// -------------------------------
-// */
+/*
+-------------------------------
+    ANBEFALINGER
+-------------------------------
+*/
 
-// app.get('/anbefalt/:id', (req, res) => { //viser alle anbefalte serier med info
-//     const motakerID = req.params.id;
-//     const mottatAnbefaling = db.prepare(`
-//         SELECT anbefaling.*, serie.*
-//         FROM anbefaling
-//         INNER JOIN serie ON anbefaling.serieID = serie.idS
-//         WHERE anbefaling.motakerID = ?
-//     `).all(motakerID);
+app.get('/anbefalt/:id', (req, res) => { //viser alle anbefalte serier med info
+    const motakerID = req.params.id;
+    const mottatAnbefaling = db.prepare(`
+        SELECT anbefaling.*, serie.*
+        FROM anbefaling
+        INNER JOIN serie ON anbefaling.serieID = serie.idS
+        WHERE anbefaling.motakerID = ?
+    `).all(motakerID);
     
-//     if (!mottatAnbefaling || mottatAnbefaling.length === 0) {
-//         return res.status(404).json({ message: "bruker ikke funnet" });
-//     }
-//     res.json(mottatAnbefaling);
-// });
+    if (!mottatAnbefaling || mottatAnbefaling.length === 0) {
+        return res.status(404).json({ message: "bruker ikke funnet" });
+    }
+    res.json(mottatAnbefaling);
+});
 
-// app.post('/anbefalAndre', (req, res) => { //viser alle serier i serie.html
-//     const { Aserie, motaker, sender, kommentar } = req.body;
+app.post('/anbefalAndre', (req, res) => { //viser alle serier i serie.html
+    const { Aserie, motaker, sender, kommentar } = req.body;
 
-//     try {//ser om den kan gjøre følgende
-//         const insert = db.prepare('INSERT INTO anbefaling (serieID, motakerID, senderID, kommentar) VALUES (?, ?, ?, ?)'); //sier hvor det skal settes inn
-//         insert.run(Aserie, motaker, sender, kommentar);//sier det skal settes inn
-//         // res.send("Konto opprettet!");
-//         res.json({ ok: true, message: "Anbefaling sendt"});
-//     } catch (error) { //hvis den ikke klarte å gjøre det så gjør den følgende
-//         console.log(error);//Error: viser frem feilmeldinger
-//         // res.send("Feil ved opprettelse");
-//         res.status(500).json({error:"Feil ved opprettelse" })
-//     }
-// });
+    try {//ser om den kan gjøre følgende
+        const insert = db.prepare('INSERT INTO anbefaling (serieID, motakerID, senderID, kommentar) VALUES (?, ?, ?, ?)'); //sier hvor det skal settes inn
+        insert.run(Aserie, motaker, sender, kommentar);//sier det skal settes inn
+        // res.send("Konto opprettet!");
+        res.json({ ok: true, message: "Anbefaling sendt"});
+    } catch (error) { //hvis den ikke klarte å gjøre det så gjør den følgende
+        console.log(error);//Error: viser frem feilmeldinger
+        // res.send("Feil ved opprettelse");
+        res.status(500).json({error:"Feil ved opprettelse" })
+    }
+});
 
-// // app.get('/anbefaltSok', (req, res) => { //viser alle serier i serie.html
-// //     const {sok} = req.body; //henter brukernavn og passord fra body
+// app.get('/anbefaltSok', (req, res) => { //viser alle serier i serie.html
+//     const {sok} = req.body; //henter brukernavn og passord fra body
     
-// //     const sendSok = db.prepare('SELECT * FROM serie.navn WHERE sok = ?').get(sok);//henter alle brukernavn og passord
-// //     if (!sendSok) { //hvis noe mangler får du failmelding
-// //         return res.status(401).json({ message: "feil ved søk"});
-// //     }
-// //     console.log(sendSok)
-// // });
+//     const sendSok = db.prepare('SELECT * FROM serie.navn WHERE sok = ?').get(sok);//henter alle brukernavn og passord
+//     if (!sendSok) { //hvis noe mangler får du failmelding
+//         return res.status(401).json({ message: "feil ved søk"});
+//     }
+//     console.log(sendSok)
+// });
 
 // // SLETTE KNAPP
 // app.delete('/anbefalt/:id', (req, res) => {
